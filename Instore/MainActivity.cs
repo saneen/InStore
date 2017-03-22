d8859306ed05814708b555aa107c4c396794cf2f
mridulpj <mridulpj@hotmail.com>
2017-03-22 11:24:42 +0530

    photogrid

--- Instore/MainActivity.cs
+++ Instore/MainActivity.cs
@@ -23,15 +23,28 @@ namespace Instore
         NavigationView navigationView;
         DrawerLayout drawerLayout;
 		private Button pickplace;
-	/*	RecyclerView mRecyclerView;
-		RecyclerView.LayoutManager mLayoutManager;
-	recycleradapter mAdapter;
-		PhotoAlbum mPhotoAlbum;
-*/
+
+        // RecyclerView instance that displays the photo album:
+        RecyclerView mRecyclerView;
+
+        // Layout manager that lays out each card in the RecyclerView:
+        RecyclerView.LayoutManager mLayoutManager, list;
+
+        // Adapter that accesses the data set (a photo album):
+        PhotoAlbumAdapter mAdapter;
+
+        // Photo album that is managed by the adapter:
+        PhotoAlbum mPhotoAlbum;
+
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-            SetContentView (Resource.Layout.Main);
+
+            // Instantiate the photo album:
+            mPhotoAlbum = new PhotoAlbum();
+
+            SetContentView(Resource.Layout.Main);
             var toolbar = FindViewById<V7Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
             SupportActionBar.SetDisplayHomeAsUpEnabled(true);
@@ -45,10 +58,68 @@ namespace Instore
 			navigationView.NavigationItemSelected+=NavigationView_NavigationItemSelected;
 			pickplace.Click += OnPickAPlaceButtonTapped;
 
+    //----------------------------------------------------------------------
+
+            // Get our RecyclerView layout:
+            mRecyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
+
+            //............................................................
+            // Layout Manager Setup:
+
+            // Use the built-in linear layout manager:
+            mLayoutManager = new LinearLayoutManager(this);
+            list = mLayoutManager;
+
+            // Plug the layout manager into the RecyclerView:
+            mRecyclerView.SetLayoutManager(mLayoutManager);
+
+            //............................................................
+            // Adapter Setup:
+
+            // Create an adapter for the RecyclerView, and pass it the
+            // data set (the photo album) to manage:
+            mAdapter = new PhotoAlbumAdapter(mPhotoAlbum);
+
+            // Register the item click handler (below) with the adapter:
+            mAdapter.ItemClick += OnItemClick;
+
+            // Plug the adapter into the RecyclerView:
+            mRecyclerView.SetAdapter(mAdapter);
+
+            //............................................................
+
+            Button cngview = FindViewById<Button>(Resource.Id.changeview);
+
+            cngview.Click += Cngview_Click;
 
         }
-		private void NavigationView_NavigationItemSelected(object sender, NavigationView.NavigationItemSelectedEventArgs e)
-		{
+
+//CHANGE VIEW BUTTON 
+
+        private void Cngview_Click(object sender, EventArgs e)
+        {
+            if (mLayoutManager == list)
+                mLayoutManager = new GridLayoutManager
+                    (this, 2, LinearLayoutManager.Vertical, false);
+            else
+                mLayoutManager = list;
+
+            mRecyclerView.SetLayoutManager(mLayoutManager);
+        }
+
+        // Handler for the item click event:
+        void OnItemClick(object sender, int position)
+        {
+            // Display a toast that briefly shows the enumeration of the selected photo:
+            int photoNum = position + 1;
+            Toast.MakeText(this, "This is photo number " + photoNum, ToastLength.Short).Show();
+        }
+
+    
+
+
+    private void NavigationView_NavigationItemSelected(object sender, NavigationView.NavigationItemSelectedEventArgs e)
+{
 			var menuitem = e.MenuItem;
 			switch (menuitem.ItemId)
 			{
@@ -143,7 +214,171 @@ namespace Instore
 		}
 		}
 
+    //----------------------------------------------------------------------
+    // VIEW HOLDER
+    public class PhotoViewHolder : RecyclerView.ViewHolder
+    {
+        public ImageView Image { get; private set; }
+        public TextView Caption { get; private set; }
 
-	}
+        // Get references to the views defined in the CardView layout.
+        public PhotoViewHolder(View itemView, Action<int> listener)
+            : base(itemView)
+        {
+            // Locate and cache view references:
+            Image = itemView.FindViewById<ImageView>(Resource.Id.imageView);
+            Caption = itemView.FindViewById<TextView>(Resource.Id.textView);
+
+            // Detect user clicks on the item view and report which item
+            // was clicked (by position) to the listener:
+            itemView.Click += (sender, e) => listener(base.Position);
+        }
+    }
+
+
+    //----------------------------------------------------------------------
+    // ADAPTER
+    public class PhotoAlbumAdapter : RecyclerView.Adapter
+    {
+        // Event handler for item clicks:
+        public event EventHandler<int> ItemClick;
+
+        // Underlying data set (a photo album):
+        public PhotoAlbum mPhotoAlbum;
+
+        // Load the adapter with the data set (photo album) at construction time:
+        public PhotoAlbumAdapter(PhotoAlbum photoAlbum)
+        {
+            mPhotoAlbum = photoAlbum;
+        }
+
+        // Create a new photo CardView (invoked by the layout manager): 
+        public override RecyclerView.ViewHolder
+            OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            // Inflate the CardView for the photo:
+            View itemView = LayoutInflater.From(parent.Context).
+                        Inflate(Resource.Layout.Photocardview, parent, false);
+
+            // Create a ViewHolder to find and hold these view references, and 
+            // register OnClick with the view holder:
+            PhotoViewHolder vh = new PhotoViewHolder(itemView, OnClick);
+            return vh;
+        }
+
+        // Fill in the contents of the photo card (invoked by the layout manager):
+        public override void
+            OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            PhotoViewHolder vh = holder as PhotoViewHolder;
+
+            // Set the ImageView and TextView in this ViewHolder's CardView 
+            // from this position in the photo album:
+            vh.Image.SetImageResource(mPhotoAlbum[position].PhotoID);
+            vh.Caption.Text = mPhotoAlbum[position].Caption;
+        }
+
+        // Return the number of photos available in the photo album:
+        public override int ItemCount
+        {
+            get { return mPhotoAlbum.NumPhotos; }
+        }
+
+        // Raise an event when the item-click takes place:
+        void OnClick(int position)
+        {
+            if (ItemClick != null)
+                ItemClick(this, position);
+        }
+    }
+
+    // Photo: contains image resource ID and caption:
+    public class Photo
+    {
+        // Photo ID for this photo:
+        public int mPhotoID;
+
+        // Caption text for this photo:
+        public string mCaption;
+
+        // Return the ID of the photo:
+        public int PhotoID
+        {
+            get { return mPhotoID; }
+        }
+
+        // Return the Caption of the photo:
+        public string Caption
+        {
+            get { return mCaption; }
+        }
+    }
+
+    // Photo album: holds image resource IDs and caption:
+    public class PhotoAlbum
+    {
+        // Built-in photo collection - this could be replaced with
+        // a photo database:
+
+        static Photo[] mBuiltInPhotos = {
+            new Photo { mPhotoID = Resource.Drawable.app_splashcreen,
+                        mCaption = "Slash" },
+            new Photo { mPhotoID = Resource.Drawable.gpsicon,
+                        mCaption = "Code" },
+            new Photo { mPhotoID = Resource.Drawable.icon_shopping,
+                        mCaption = "The Logo" },
+            new Photo { mPhotoID = Resource.Drawable.ic_menu,
+                        mCaption = "InStore" },
+            new Photo { mPhotoID = Resource.Drawable.app_splashcreen,
+                        mCaption = "Slash" },
+            new Photo { mPhotoID = Resource.Drawable.gpsicon,
+                        mCaption = "Code" },
+            new Photo { mPhotoID = Resource.Drawable.icon_shopping,
+                        mCaption = "The Logo" },
+            new Photo { mPhotoID = Resource.Drawable.ic_menu,
+                        mCaption = "InStore" },
+            new Photo { mPhotoID = Resource.Drawable.app_splashcreen,
+                        mCaption = "Slash" },
+            new Photo { mPhotoID = Resource.Drawable.gpsicon,
+                        mCaption = "Code" },
+            new Photo { mPhotoID = Resource.Drawable.icon_shopping,
+                        mCaption = "The Logo" },
+            new Photo { mPhotoID = Resource.Drawable.ic_menu,
+                        mCaption = "InStore" },
+            new Photo { mPhotoID = Resource.Drawable.app_splashcreen,
+                        mCaption = "Slash" },
+            new Photo { mPhotoID = Resource.Drawable.gpsicon,
+                        mCaption = "Code" },
+            new Photo { mPhotoID = Resource.Drawable.icon_shopping,
+                        mCaption = "The Logo" },
+            new Photo { mPhotoID = Resource.Drawable.ic_menu,
+                        mCaption = "InStore" },
+
+            };
+
+        // Array of photos that make up the album:
+        private Photo[] mPhotos;
+
+        // Create an instance copy of the built-in photo list
+        public PhotoAlbum()
+        {
+            mPhotos = mBuiltInPhotos;
+        }
+
+        // Return the number of photos in the photo album:
+        public int NumPhotos
+        {
+            get { return mPhotos.Length; }
+        }
+
+        // Indexer (read only) for accessing a photo:
+        public Photo this[int i]
+        {
+            get { return mPhotos[i]; }
+        }
+    } 
+
+
+}
 
 
